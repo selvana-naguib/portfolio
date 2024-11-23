6edc1dbb065d4d065521e9c7118f2a3f9e837ca7
selvana-naguib <selvana.naguib4@gmail.com>
2024-11-23 16:24:44 +0200

    Gallery
    Gallery

---
+++ Old Template/js/galleries.js
@@ -0,0 +1,174 @@
+projects = [];
+pendingFolderCount = 0;
+notProjects = [".DS_Store", ""];
+i = -1;
+
+
+function loadProjects(foldername, category) {
+    incrementPendingFolderCount();
+    $.ajax({
+        url: foldername,
+        success: function (foldersData) {
+            //addRow("AdminDryclean","AdminDryclean",1,864,"864 B",1628262972,"8/6/21, 5:16:12 PM");
+            folders = foldersData.matchAll(/addRow\("([\w\d\s-_\.\(\)&\[\]$–—,'’]*)"*/g);
+            for (folder of folders) {
+                if (!notProjects.includes(folder[1])) {
+
+                    i++;
+                    projects[i] = {};
+                    projects[i].name = folder[1];
+                    projects[i].images = [];
+                    projects[i].category = category;
+                    projects[i].folder = foldername + projects[i].name + '/'
+                    incrementPendingFolderCount();
+                    $.ajax({
+                        url: projects[i].folder,
+                        indexValue: i,
+                        success: function (imagesData) {
+                            images = imagesData.matchAll(/addRow\("([\w\d\s-_\.\(\)&\[\]$–—,'’\?/\/:]*)"*/g);
+                            j = 0;
+                            for (image of images) {
+                                if (!notProjects.includes(image[1]) && !image[1].includes('-thumbnail')) {
+                                    projects[this.indexValue].images[j++] = image[1];
+                                }
+                            }
+                            deccrementPendingFolderCount();
+                        },
+                        error: function(XMLHttpRequest, textStatus, errorThrown) {deccrementPendingFolderCount();}
+                    });
+                }
+            }
+            deccrementPendingFolderCount();
+            // console.log(projects);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {deccrementPendingFolderCount();}
+    });
+}
+
+function loadScript(scriptName) {
+    element = document.createElement("script");
+    element.src = scriptName;
+    document.body.appendChild(element);
+}
+
+function loadAllScripts() {
+    //    loadScript('js/nerveslider.min.js');
+    // loadScript('js/wookmark.js');
+    loadScript('js/portfolio-trigger.js');
+    //   loadScript('js/jquery.tooltipster.min.js');
+    //   loadScript('js/quovolver.js');
+    //loadScript('js/scrollbar.js');
+    loadScript('js/lightgallery.js');
+    //   loadScript('js/googlemap.js');
+    //   loadScript('js/home-custom.js');
+    //   loadScript('js/sendmail.js');
+}
+
+function sortProjects() {
+    projects.sort(function (a, b) {
+        if (a.name.toUpperCase() < b.name.toUpperCase()) {
+            return -1;
+        }
+        if (a.name.toUpperCase() > b.name.toUpperCase()) {
+            return 1;
+        }
+        return 0;
+    });
+
+    for (index = 0; index < projects.length; index++) {
+        projects[index].id = index;
+    }
+    console.log(projects);
+}
+
+function incrementPendingFolderCount() {
+    pendingFolderCount++;
+    console.log('inc');
+}
+
+function deccrementPendingFolderCount() {
+    pendingFolderCount--;
+    console.log('dec');
+}
+
+function loadProjectsfromFile() {
+    incrementPendingFolderCount();
+    $.getJSON('projects.json', function(data) {         
+        projects = data;
+    }).always(function() { deccrementPendingFolderCount(); });;
+}
+loadScript('js/wookmark.js');
+
+// loadProjects('projects/wireframes/', 'cat1');
+// loadProjects('projects/websites/', 'cat2');
+// loadProjects('projects/mobileApplications/', 'cat3');
+// loadProjects('projects/designs/', 'cat4');
+// loadProjects('projects/diagrams/', 'cat5');
+loadProjectsfromFile();
+interval = setInterval(() => {
+        console.log(pendingFolderCount);
+        jQuery("body").find('#site-loading').fadeIn(500);
+        if (pendingFolderCount === 0) {
+           console.log('count is zero');
+           console.log(JSON.stringify(projects));   
+           clearInterval(interval);
+           console.log('continue');
+            jQuery("body").find('#site-loading').fadeOut(500);
+            sortProjects();
+            for (project of projects) {
+                if (project.images.length > 0) {
+                    //console.log("project name: ", project.name);
+                    $("#cv-portfolio").append(`
+            <li data-filter-class='["all","${project.category}"]'>
+                <figure>
+                    <a id="gallery${project.id}" href="#" class="cvgrid-img">
+                        <img src="${project.folder}${project.images[0].replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}" alt="" />
+                    </a>
+                    <figcaption>
+                        <div class="cvgrid-title">${!project.name.includes(".") ? project.name : project.name.split(".")[1]}</div>
+                    </figcaption>
+                </figure>
+            </li>
+            `);
+                }
+            }
+                loadAllScripts();
+
+                for (project of projects) {
+                    if (project.images.length > 0) {
+                        jQuery('#gallery' + project.id).on('click', function (e) {
+                            "use strict";
+                            e.preventDefault();
+                            project = projects[+e.currentTarget.id.replace('gallery', '')];
+                            console.log("gallery: ", +e.currentTarget.id.replace('gallery', ''));
+                            console.log("project: ", project);
+                            if (project.images[0].includes('link$')) {
+                                window.location.href = project.images[0].replace('link$www', 'http://www').replace('link$', '').replaceAll(':', '/').replace('.png', '');
+                            } else {
+                                var galleryimages = [];
+                                for (let image of project.images) {
+                                    galleryimages.push({
+                                        'src': `${project.folder}${image}`,
+                                        'thumb': `${project.folder}${image.replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}`,
+                                        'subHtml': `${image.split(".")[0]}`
+                                    });
+                                }
+                                jQuery(this).lightGallery({
+                                    dynamic: true,
+                                    mode: 'lg-slide-vertical',
+                                    zoom: true,
+                                    fullScreen: true,
+                                    autoplay: false,
+                                    thumbnail: false,
+                                    download: true,
+                                    counter: true,
+                                    // Images   
+                                    dynamicEl: galleryimages
+                                })
+                            }
+                        });
+                    }
+                }
+
+    }
+}, 1000);
--- js/galleries.js
+++ js/galleries.js
@@ -1,67 +1,100 @@
-projects = [];
-pendingFolderCount = 0;
-notProjects = [".DS_Store", ""];
-i = -1;
+var categories = {};
+var projects;
+categoriesOrder = [];
+function getCategoryName (category) {
+    switch(category) {
+        case "cat1":
+          return "Wireframes"
+        case "cat2":
+          return "Websites"
+        case "cat3":
+          return "Mobile Applications"
+        case "cat4":
+          return "Designs"
+        case "cat5":
+          return "Diagrams"
+        case "cat6":
+          return "Design Systems"
+        default: console.log(category)
+      }
+}
 
+function renderCategories() {
+    // Add all tab
+    $(".album-tab").append(`<a href="#album-all" class="is-active">All</a>`);
+    $(".content-inner").append(`
+        <div class="grid-album" id="album-all">
+            <div class="grid-sizer"></div>
+        </div>`);
+    for (i in projects) {
+        project = projects[i];
+        $(`#album-all`).append(`
+        <div class="grid-item album-item">
+            <a href="gallery-single.html?project=${project.id}">
+                <div class="album-thumb">
+                    <img src="${project.folder}${project.images[0].replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}" 
+                    class="" alt="${!project.name.includes(".") ? project.name : project.name.split(".")[1]}">
+                </div>
+                <div class="album-name">${!project.name.includes(".") ? project.name : project.name.split(".")[1]}</div>
+            </a>
+        </div>`);
+    }
 
-function loadProjects(foldername, category) {
-    incrementPendingFolderCount();
-    $.ajax({
-        url: foldername,
-        success: function (foldersData) {
-            //addRow("AdminDryclean","AdminDryclean",1,864,"864 B",1628262972,"8/6/21, 5:16:12 PM");
-            folders = foldersData.matchAll(/addRow\("([\w\d\s-_\.\(\)&\[\]$–—,'’]*)"*/g);
-            for (folder of folders) {
-                if (!notProjects.includes(folder[1])) {
+    // add categories tabs
+    Object.entries(categories).forEach(([key, value]) => {
+        $(".album-tab").append(`<a href="#album-${key}" >${getCategoryName(key)}</a>`);
+        $(".content-inner").append(`
+        <div class="grid-album" id="album-${key}">
+            <div class="grid-sizer"></div>
+        </div>`)
 
-                    i++;
-                    projects[i] = {};
-                    projects[i].name = folder[1];
-                    projects[i].images = [];
-                    projects[i].category = category;
-                    projects[i].folder = foldername + projects[i].name + '/'
-                    incrementPendingFolderCount();
-                    $.ajax({
-                        url: projects[i].folder,
-                        indexValue: i,
-                        success: function (imagesData) {
-                            images = imagesData.matchAll(/addRow\("([\w\d\s-_\.\(\)&\[\]$–—,'’\?/\/:]*)"*/g);
-                            j = 0;
-                            for (image of images) {
-                                if (!notProjects.includes(image[1]) && !image[1].includes('-thumbnail')) {
-                                    projects[this.indexValue].images[j++] = image[1];
-                                }
-                            }
-                            deccrementPendingFolderCount();
-                        },
-                        error: function(XMLHttpRequest, textStatus, errorThrown) {deccrementPendingFolderCount();}
-                    });
-                }
-            }
-            deccrementPendingFolderCount();
-            // console.log(projects);
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {deccrementPendingFolderCount();}
+        for (p in value) {
+            project = value[p]
+            $(`#album-${key}`).append(`
+            <div class="grid-item album-item">
+                <a href="gallery-single.html?project=${project.id}">
+                    <div class="album-thumb">
+                        <img src="${project.folder}${project.images[0].replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}" 
+                        class="" alt="${!project.name.includes(".") ? project.name : project.name.split(".")[1]}">
+                    </div>
+                    <div class="album-name">${!project.name.includes(".") ? project.name : project.name.split(".")[1]}</div>
+                </a>
+            </div>`);
+        }
+    
     });
+
+    // click on first tab
+    setTimeout(() => {
+        $(`.album-tab a:first`).click();
+    }, 100);
 }
 
-function loadScript(scriptName) {
-    element = document.createElement("script");
-    element.src = scriptName;
-    document.body.appendChild(element);
+function renderProject(p){
+    project = projects[p];
+    // $(".grid-item").remove();
+    for (i in project.images) {
+        $(".grid-single").append(`
+            <div class="grid-item"><a href="${project.folder}${project.images[i]}" data-effect="mfp-zoom-in"><img class="b-lazy" src="${project.folder}${project.images[i].replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}" alt="${project.images[i]}"></a></div>
+        `);    
+    } 
+    $(".title").text(!project.name.includes(".") ? project.name : project.name.split(".")[1]);
+    $(".subtitle").find("p").text(project.desc ? project.desc : "");
+    $("#goNext").attr("href", goNext(p));
+    $("#goPrevious").attr("href", goPrevious(p));
 }
 
-function loadAllScripts() {
-    //    loadScript('js/nerveslider.min.js');
-    // loadScript('js/wookmark.js');
-    loadScript('js/portfolio-trigger.js');
-    //   loadScript('js/jquery.tooltipster.min.js');
-    //   loadScript('js/quovolver.js');
-    //loadScript('js/scrollbar.js');
-    loadScript('js/lightgallery.js');
-    //   loadScript('js/googlemap.js');
-    //   loadScript('js/home-custom.js');
-    //   loadScript('js/sendmail.js');
+function goNext (p) {
+    n = (Number(p) +1)
+    if (n>=projects.length) n=0;
+    return "gallery-single.html?project="+n;
+}
+
+function goPrevious (p) {
+    n = Number(p) - 1;
+    if (n < 0) n = projects.length -1
+    return "gallery-single.html?project="+n;
+
 }
 
 function sortProjects() {
@@ -74,101 +107,40 @@ function sortProjects() {
         }
         return 0;
     });
+}
 
-    for (index = 0; index < projects.length; index++) {
-        projects[index].id = index;
+function loadScript(scriptName) {
+    element = document.createElement("script");
+    element.src = scriptName;
+    document.body.appendChild(element);
+}
+
+function loadAllScripts() {
+    loadScript("js/swiper-bundle.min.js");
+    loadScript("js/wow.min.js");
+    loadScript("js/masonry.pkgd.min.js");
+    loadScript("js/imagesloaded.pkgd.min.js");
+    loadScript("js/magnific-popup.min.js");
+    loadScript("js/blazy.min.js");
+    loadScript("js/parazoom.min.js");
+    loadScript("js/script.js");
+}
+
+$.getJSON('projects.json', function(data) {  
+    projects = data; 
+    sortProjects();
+    for (p in projects) {
+        project = projects[p]
+        key = project.category;
+        if (!categories[key]) categories[key] = []
+        project.id = p;
+        categories[key].push(project);
     }
-    console.log(projects);
-}
-
-function incrementPendingFolderCount() {
-    pendingFolderCount++;
-    console.log('inc');
-}
-
-function deccrementPendingFolderCount() {
-    pendingFolderCount--;
-    console.log('dec');
-}
-
-function loadProjectsfromFile() {
-    incrementPendingFolderCount();
-    $.getJSON('projects.json', function(data) {         
-        projects = data;
-    }).always(function() { deccrementPendingFolderCount(); });;
-}
-loadScript('js/wookmark.js');
-
-// loadProjects('projects/wireframes/', 'cat1');
-// loadProjects('projects/websites/', 'cat2');
-// loadProjects('projects/mobileApplications/', 'cat3');
-// loadProjects('projects/designs/', 'cat4');
-// loadProjects('projects/diagrams/', 'cat5');
-loadProjectsfromFile();
-interval = setInterval(() => {
-        console.log(pendingFolderCount);
-        jQuery("body").find('#site-loading').fadeIn(500);
-        if (pendingFolderCount === 0) {
-           console.log('count is zero');
-           console.log(JSON.stringify(projects));   
-           clearInterval(interval);
-           console.log('continue');
-            jQuery("body").find('#site-loading').fadeOut(500);
-            sortProjects();
-            for (project of projects) {
-                if (project.images.length > 0) {
-                    //console.log("project name: ", project.name);
-                    $("#cv-portfolio").append(`
-            <li data-filter-class='["all","${project.category}"]'>
-                <figure>
-                    <a id="gallery${project.id}" href="#" class="cvgrid-img">
-                        <img src="${project.folder}${project.images[0].replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}" alt="" />
-                    </a>
-                    <figcaption>
-                        <div class="cvgrid-title">${!project.name.includes(".") ? project.name : project.name.split(".")[1]}</div>
-                    </figcaption>
-                </figure>
-            </li>
-            `);
-                }
-            }
-                loadAllScripts();
-
-                for (project of projects) {
-                    if (project.images.length > 0) {
-                        jQuery('#gallery' + project.id).on('click', function (e) {
-                            "use strict";
-                            e.preventDefault();
-                            project = projects[+e.currentTarget.id.replace('gallery', '')];
-                            console.log("gallery: ", +e.currentTarget.id.replace('gallery', ''));
-                            console.log("project: ", project);
-                            if (project.images[0].includes('link$')) {
-                                window.location.href = project.images[0].replace('link$www', 'http://www').replace('link$', '').replaceAll(':', '/').replace('.png', '');
-                            } else {
-                                var galleryimages = [];
-                                for (let image of project.images) {
-                                    galleryimages.push({
-                                        'src': `${project.folder}${image}`,
-                                        'thumb': `${project.folder}${image.replace('.png','-thumbnail.png').replace('.jpg','-thumbnail.jpg').replace('.PNG','-thumbnail.PNG').replace('.JPG','-thumbnail.JPG')}`,
-                                        'subHtml': `${image.split(".")[0]}`
-                                    });
-                                }
-                                jQuery(this).lightGallery({
-                                    dynamic: true,
-                                    mode: 'lg-slide-vertical',
-                                    zoom: true,
-                                    fullScreen: true,
-                                    autoplay: false,
-                                    thumbnail: false,
-                                    download: true,
-                                    counter: true,
-                                    // Images   
-                                    dynamicEl: galleryimages
-                                })
-                            }
-                        });
-                    }
-                }
-
-    }
-}, 1000);
+    if (window.location.href.indexOf("gallery.html") > -1)  renderCategories();
+    else {
+        const urlParams = new URLSearchParams(window.location.search);
+        const p  = urlParams.get('project');
+        if (p) renderProject(p);
+    } 
+    loadAllScripts();
+});
